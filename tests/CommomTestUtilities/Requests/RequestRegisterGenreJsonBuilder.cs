16c38129a752af362aff98fcf0a0275f108342e5
Maaluu21 <marialuizapaes22@gmail.com>
2026-01-18 16:51:21 -0300

    test - test genre name

---
+++ tests/CommomTestUtilities/Requests/RequestRegisterGenreJsonBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Bogus;
+using LibraryOn.Communication.Requests.Genres;
+
+namespace CommomTestUtilities.Requests;
+public class RequestRegisterGenreJsonBuilder
+{
+    public static RequestGenreJson Build()
+    {
+        return new Faker<RequestGenreJson>()
+            .RuleFor(g => g.Name, faker => faker.Person.FirstName);
+    }
+}
